66d514675d7b6ecf2b004f48a44a146ec1bae38e
AmirMohammad_Rezaei <amirrezaei1133@gmail.com>
2021-12-02 18:59:39 +0330

    in  this version add custom material button

--- lib/generalwidgets.dart
+++ lib/generalwidgets.dart
@@ -3,19 +3,34 @@ library generalwidgets;
 import 'package:flutter/material.dart';
 
 class AmR_MaterialButton extends StatelessWidget {
-  AmR_MaterialButton(
-      {required this.backcolor, required this.onpress, this.padding, Key? key})
-      : super(key: key);
+  AmR_MaterialButton({
+    required this.backcolor,
+    required this.onpress,
+    this.padding,
+    this.borderradius = 0,
+    this.borderside = 0,
+    required this.child,
+    this.elevation = 0,
+    Key? key,
+  }) : super(key: key);
   final Color? backcolor;
   EdgeInsets? padding;
   final VoidCallback onpress;
+  final double? borderradius;
+  double? borderside;
+  final Widget child;
+  final double? elevation;
 
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(borderradius!)),
       onPressed: onpress,
       color: backcolor,
       padding: padding ?? null,
+      child: child,
+      elevation: elevation,
     );
   }
 }
