b4bbcfb3e812c9f79927218e45b8bdce3a89210b
AmirMohammad_Rezaei <amirrezaei1133@gmail.com>
2021-12-02 18:15:25 +0330

    AMRtextformfield widgtes added

---
+++ lib/AMRTextFormField.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AMRTextFormField extends StatelessWidget {
+  const AMRTextFormField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField();
+  }
+}
